f39720f3c74608e71866fbe1cddbe2a4abb94d7e
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2019-03-18 17:49:54 -0300

    chau board

---
+++ src/Stack/Game.java
@@ -0,0 +1,7 @@
+package Stack;
+
+public class Game {
+
+    private Horse[][] board;
+
+}
---
+++ src/Stack/Horse.java
@@ -0,0 +1,12 @@
+package Stack;
+
+public class Horse {
+
+    private Stack<String>[] movement;
+
+    public Horse() {
+        movement = new Stack[4];
+    }
+
+
+}
