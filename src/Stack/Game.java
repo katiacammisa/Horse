fd39f1d4f8be180bd23ed3ce09ec3177eae1b968
Katy <katia.cammisa@ing.austral.edu.ar>
2019-03-18 20:23:38 -0300

    puta

--- src/Stack/Game.java
+++ src/Stack/Game.java
@@ -27,31 +27,35 @@ public class Game {
         //posibilidades por lugar
         //llenar el stack
         //imprimir
-        Position currentPosition = new Position(0, 0);
 
-        for (int i = 0; i < board.length ; i++) {
-            for (int j = 0; j < board[i].length ; j++) {
-                if(board[i][j]){
-                    currentPosition = new Position(i, j);
+        int number = 0;
+        while(number <398) {
+
+            Position currentPosition = new Position(0, 0);
+
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (board[i][j]) {
+                        currentPosition = new Position(i, j);
+                    }
                 }
             }
-        }
 
-        List<Position> possibilities = horse.whereToJump(currentPosition);
+            List<Position> possibilities = horse.whereToJump(currentPosition);
 
-        for (int i = 0; i < piles.length; i++) {
-            if(piles[i].isEmpty()) {
-                for (int j = 0; j < possibilities.size(); j++) {
-                    piles[i].push(possibilities.get(j).toString());
+            for (int i = 0; i < piles.length; i++) {
+                if (piles[i].isEmpty()) {
+                    for (int j = 0; j < possibilities.size(); j++) {
+                        piles[i].push(possibilities.get(j).toString());
+                    }
+                    board[new Position(piles[i].peek()).getX()][new Position(piles[i].peek()).getY()] = true;
+
                 }
-                board[new Position(piles[i].peek()).getX()][new Position(piles[i].peek()).getY()] = true;
-
             }
+
+            board[currentPosition.getX()][currentPosition.getY()] = false;
+
+            ++number;
         }
-
-        board[currentPosition.getX()][currentPosition.getY()] = false;
-
-        System.out.println(possibilities.toString());
     }
-
 }
