239735ae341d19c2d6cd60eb6b1ba98247e0b40f
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2019-03-21 08:41:02 -0300

    rename

--- src/Stack/Game.java
+++ src/Stack/Game.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-
     private boolean[][] board = new boolean[8][8];
     private Horse horse;
     private Stack<String>[] piles;
@@ -25,64 +24,71 @@ public class Game {
         currentPosition = new Position(0,0);
     }
 
+    private void fillOnePile(){
+
+        List<Position> possibilities = horse.whereToJump(currentPosition);
+
+
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i].isEmpty()) {
+                for (int j = 0; j < possibilities.size(); j++) {
+                    piles[i].push(possibilities.get(j).toString());
+                }
+                board[new Position(piles[i].peek()).getX()][new Position(piles[i].peek()).getY()] = true;
+                board[currentPosition.getX()][currentPosition.getY()] = false;
+                break;
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j]) {
+                    currentPosition = new Position(i, j);
+                }
+            }
+        }
+    }
+
+    public Position deserialize(String position){
+        return new Position(Integer.parseInt(position.split(",")[0]), Integer.parseInt(position.split(",")[1]));
+    }
 
     public void horsePossibleJumps() {
-        //posibilidades por lugar
-        //imprimir
         List<Possibility> possibilityList = new ArrayList<Possibility>();
 
-            while(piles[3].isEmpty()){
-                //fillOnePile();
-                for (int i = 0; i < board.length; i++) {
-                    for (int j = 0; j < board[i].length; j++) {
-                        if (board[i][j]) {
-                            currentPosition = new Position(i, j);
-                        }
+        while(piles[3].isEmpty()){
+            fillOnePile();
+        }
+
+        while(!piles[0].isEmpty()){
+            currentPosition = deserialize(piles[0].peek());
+            board[currentPosition.getX()][currentPosition.getY()] = true;
+            fillOnePile();
+            while (!piles[1].isEmpty()){
+                currentPosition = deserialize(piles[1].peek());
+                board[currentPosition.getX()][currentPosition.getY()] = true;
+                fillOnePile();
+                while (!piles[2].isEmpty()) {
+                    currentPosition = deserialize(piles[2].peek());
+                    board[currentPosition.getX()][currentPosition.getY()] = true;
+                    fillOnePile();
+                    while (!piles[3].isEmpty()) {
+                        currentPosition = deserialize(piles[3].peek());
+                        board[currentPosition.getX()][currentPosition.getY()] = true;
+                        possibilityList.add(new Possibility(new Position(piles[0].peek()),
+                                new Position(piles[1].peek()),
+                                new Position(piles[2].peek()),
+                                new Position(piles[3].peek())));
+                        piles[3].pop();
                     }
+                    piles[2].pop();
                 }
+                piles[1].pop();
             }
-
-            int number = 3;
-
-            while(number >= 0 && !piles[number].isEmpty()) {
-                if(piles[number].isEmpty()){
-                    number--;
-
-                }
-                possibilityList.add(new Possibility(new Position(piles[0].peek()),
-                        new Position(piles[1].peek()),
-                        new Position(piles[2].peek()),
-                        new Position(piles[3].peek())));
-                piles[number].pop();
-                while(piles[number].isEmpty()){
-                    piles[number].pop();
-                    //fillOnePile();
-                    for (int i = 0; i < board.length; i++) {
-                        for (int j = 0; j < board[i].length; j++) {
-                            if (board[i][j]) {
-                                currentPosition = new Position(i, j);
-                            }
-                        }
-                    }
-                }
-            }
+            piles[0].pop();
+        }
 
         System.out.println(possibilityList.toString());
     }
 
-//    private void fillOnePile(){
-//
-//        List<Position> possibilities = horse.whereToJump(currentPosition);
-//
-//        for (int i = 0; i < piles.length; i++) {
-//            if (piles[i].isEmpty()) {
-//                for (int j = 0; j < possibilities.size(); j++) {
-//                    piles[i].push(possibilities.get(j).toString());
-//                }
-//                board[new Position(piles[i].peek()).getX()][new Position(piles[i].peek()).getY()] = true;
-//                board[currentPosition.getX()][currentPosition.getY()] = false;
-//                break;
-//            }
-//        }
-//    }
 }
--- src/Stack/GameTest.java
+++ src/Stack/GameTest.java
@@ -3,13 +3,10 @@ package Stack;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-import static org.junit.Assert.*;
-
-public class GameTest2 extends Assert {
+public class GameTest extends Assert {
 
     Horse horse = new Horse();
-    Game2 game = new Game2(horse);
+    Game game = new Game(horse);
 
     @Test
     public void horsePossibleJumps() {
