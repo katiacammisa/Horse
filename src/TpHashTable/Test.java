6e9feda8fa89c9b04b45e2893bd42c0a8c344b81
Katy <katia.cammisa@ing.austral.edu.ar>
2019-05-24 23:39:20 -0300

    should work

--- src/TpHashTable/Test.java
+++ src/TpHashTable/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        File file = new File("Hola");
+        File file = new File("NoHola");
         Dictionary dic = new Dictionary(20,file);
     }
 }
